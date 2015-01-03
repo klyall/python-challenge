988734fce56961067917c2cf827721217f2bb166
Keith Lyall <keith.lyall@gmail.com>
2015-01-03 17:02:02 +0000

    Add level 9.

--- common.py
+++ common.py
@@ -1,7 +1,7 @@
 import webbrowser, urllib2, re
 
-def createURL(name):
-	return "http://www.pythonchallenge.com/pc/def/%s.html" % (name, )
+def createURL(name, folder='def'):
+	return "http://www.pythonchallenge.com/pc/%s/%s.html" % (folder, name)
 
 def loadPage(name): 
 	page = urllib2.urlopen(createURL(name))
@@ -13,8 +13,8 @@ def loadPageAndExtractText(name, pattern):
 	result =  regex.search(page).groups()
 	return result[0]
 
-def openNextPage(name):
+def openNextPage(name, folder='def'):
 	print "The next level is '%s'" % (name, )
-	webbrowser.open(createURL(name))
-	src_url = "http://www.pythonchallenge.com/pcc/def/%s.html" % (name, )
+	webbrowser.open(createURL(name, folder))
+	src_url = "http://www.pythonchallenge.com/pcc/%s/%s.html" % (folder, name)
 	webbrowser.open(src_url)
